1bd0cba353850df4eae97a3b040e3de8220f7f84
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-02-09 00:01:59 -0800

    Add bucket

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,9 +1,12 @@
 import {Construct, Stack, StackProps} from "monocdk";
+import {Bucket} from "monocdk/aws-s3";
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-
+    new Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
   }
 }
