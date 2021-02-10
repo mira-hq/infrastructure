8400df1aaca7daf743b6ca75926f61683dbb122c
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-02-09 21:01:38 -0800

    run prettier

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,12 +1,12 @@
-import {Construct, Stack, StackProps} from "monocdk";
-import {Bucket} from "monocdk/aws-s3";
+import { Construct, Stack, StackProps } from "monocdk";
+import { Bucket } from "monocdk/aws-s3";
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, 'MyFirstBucket', {
-      versioned: true
+    new Bucket(this, "MyFirstBucket", {
+      versioned: true,
     });
   }
 }
