d36d8a0d4aaa62696e34d03aa00659b9f98c7aa8
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-02-10 19:56:54 -0800

    Fix syntax

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -9,12 +9,12 @@ import {
   Distribution,
   HttpVersion,
   OriginAccessIdentity,
-  ViewerProtocolPolicy,
+  ViewerProtocolPolicy
 } from "monocdk/aws-cloudfront";
 import { ARecord, HostedZone, RecordTarget } from "monocdk/aws-route53";
 import {
   Certificate,
-  CertificateValidation,
+  CertificateValidation
 } from "monocdk/aws-certificatemanager";
 import { S3Origin } from "monocdk/aws-cloudfront-origins";
 import { CloudFrontTarget } from "monocdk/aws-route53-targets";
@@ -30,7 +30,7 @@ export class InfrastructureStack extends Stack {
         new PolicyStatement({
           effect: Effect.ALLOW,
           resources: ["*"],
-          actions: ["cloudformation:*"],
+          actions: ["cloudformation:*"]
         }),
         new PolicyStatement({
           effect: Effect.ALLOW,
@@ -38,38 +38,38 @@ export class InfrastructureStack extends Stack {
           actions: ["*"],
           conditions: {
             "ForAnyValue:StringEquals": {
-              "aws:CalledVia": ["cloudformation.amazonaws.com"],
-            },
-          },
+              "aws:CalledVia": ["cloudformation.amazonaws.com"]
+            }
+          }
         }),
         new PolicyStatement({
           effect: Effect.ALLOW,
           resources: ["arn:aws:s3:::cdktoolkit-stagingbucket-*"],
-          actions: ["s3:*"],
-        }),
-      ],
+          actions: ["s3:*"]
+        })
+      ]
     });
 
     new User(this, "CdkDeploymentUser", {
       userName: "CdkDeploymentUser",
-      managedPolicies: [policy],
+      managedPolicies: [policy]
     });
 
     const blockPublicAccess = new BlockPublicAccess({
       blockPublicAcls: true,
       blockPublicPolicy: true,
       ignorePublicAcls: true,
-      restrictPublicBuckets: true,
+      restrictPublicBuckets: true
     });
 
     const bucket = new Bucket(this, "FrontEndBucket", {
       versioned: true,
-      blockPublicAccess,
+      blockPublicAccess
     });
 
     const comment = "mira-hq-frontend";
     const identity = new OriginAccessIdentity(this, "OriginAccessIdentity", {
-      comment,
+      comment
     });
 
     bucket.addToResourcePolicy(
@@ -77,27 +77,27 @@ export class InfrastructureStack extends Stack {
         effect: Effect.ALLOW,
         principals: [identity.grantPrincipal],
         actions: ["s3:GetObject"],
-        resources: [bucket.bucketArn + "/*"],
+        resources: [bucket.bucketArn + "/*"]
       })
     );
 
     const domainName = "mira-hq.com";
 
     const hostedZone = new HostedZone(this, "HostedZone", {
-      zoneName: domainName,
+      zoneName: domainName
     });
 
     const certificate = new Certificate(this, "Certificate", {
       domainName,
       subjectAlternativeNames: ["*." + domainName],
-      validation: CertificateValidation.fromDns(hostedZone),
+      validation: CertificateValidation.fromDns(hostedZone)
     });
 
     const cachePolicy = new CachePolicy(this, "CachePolicy", {
       defaultTtl: Duration.minutes(5),
       cookieBehavior: CacheCookieBehavior.none(),
       headerBehavior: CacheHeaderBehavior.none(),
-      queryStringBehavior: CacheQueryStringBehavior.none(),
+      queryStringBehavior: CacheQueryStringBehavior.none()
     });
 
     const distribution = new Distribution(this, "Distribution", {
@@ -105,11 +105,11 @@ export class InfrastructureStack extends Stack {
       comment,
       defaultBehavior: {
         origin: new S3Origin(bucket, {
-          originAccessIdentity: identity,
+          originAccessIdentity: identity
         }),
         compress: true,
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        cachePolicy: cachePolicy,
+        cachePolicy: cachePolicy
       },
       defaultRootObject: "index.html",
       enableIpv6: true,
@@ -121,20 +121,36 @@ export class InfrastructureStack extends Stack {
           ttl: Duration.minutes(5),
           responsePagePath: "/index.html",
           responseHttpStatus: 200,
-          httpStatus: 404,
+          httpStatus: 404
         },
         {
           ttl: Duration.minutes(5),
           responsePagePath: "/index.html",
           responseHttpStatus: 200,
-          httpStatus: 403,
-        },
-      ],
+          httpStatus: 403
+        }
+      ]
     });
 
     new ARecord(this, "RecordSet", {
       zone: hostedZone,
-      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution))
+    });
+
+    const s3Policy = new ManagedPolicy(this, "S3DeploymentPolicy", {
+      statements: [
+
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          resources: [bucket.bucketArn + "/*"],
+          actions: ["s3:*", "s3:ListBucket"]
+        })
+      ]
+    });
+
+    new User(this, "S3DeploymentUser", {
+      userName: "S3DeploymentUser",
+      managedPolicies: [s3Policy]
     });
   }
 }
