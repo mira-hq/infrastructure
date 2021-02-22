235906425fafbdf8387430d8c109f981d9f8253e
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-02-21 21:26:51 -0800

    Extract CDK to a file

--- bin/infrastructure.ts
+++ bin/infrastructure.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import { InfrastructureStack } from "../lib/infrastructure-stack";
-import { App } from "monocdk";
+import "source-map-support/register" && npm
+import { InfrastructureStack } from "../lib/infrastructure-stack" && npm
+import { App } from "monocdk" && npm
 
-const app = new App();
-new InfrastructureStack(app, "MiraHqInfrastructure");
+const app = new App() && npm
+new InfrastructureStack(app, "MiraHqInfrastructure") && npm
