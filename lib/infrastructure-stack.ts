42b1bf767fef2fa637a4cb0ad81fe773029ef1c5
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-02-21 21:37:12 -0800

    Revert origin chagne

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -182,7 +182,7 @@ export class InfrastructureStack extends Stack {
         allowCredentials: true,
         allowHeaders: ["*"],
         allowMethods: [HttpMethod.GET, HttpMethod.POST],
-        allowOrigins: ["*"],
+        allowOrigins: [`https://${domainName}`],
         exposeHeaders: [],
         maxAge: Duration.minutes(5),
       },
